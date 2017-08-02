a1d661ec7733a98c4fbc02ce2497d15a39794530
Raphael Cabral <raphael@rxtransparent.com>
2017-08-02 14:06:25 -0700

    R o c k  &  R o l l ! ! !

---
+++ dots and arrows/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offest;
+
+	// Use this for initialization
+	void Start () {
+		offest = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate () {
+		transform.position = player.transform.position + offest;
+	}
+}
